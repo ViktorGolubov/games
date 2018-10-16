554690827fd3210c0b847afc909fc95542a34b46
Viktor Golubov <golubovviktor@mail.ru>
2018-10-16 16:40:06 +0200

    Tried to make task for extra points

--- src/main/java/tech/bts/games/Main.java
+++ src/main/java/tech/bts/games/Main.java
@@ -1,5 +1,8 @@
 package tech.bts.games;
+import util.StringUtil;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -9,8 +12,7 @@ public class Main {
         Game game1 = new Game("Football", 22, 12);
         Game game2 = new Game("Golf", 1, 50);
         Game game3 = new Game("Tennis", 2, 15);
-        Game game4 = new Game("Ping-pong", 2, 0);
-        Game game5 = new Game("Padal", 4, 7);
+
 
 
         List<Game> games = new ArrayList<Game>();
@@ -18,18 +20,45 @@ public class Main {
         games.add(game1);
         games.add(game2);
         games.add(game3);
-        games.add(game4);
-        games.add(game5);
+
+        int columnWidth = 10;
+
+        List<String> fieldNames = Arrays.asList("Name", "Players", "Price");
+
+        String boarder = "|";
+        String title = "";
+        for (String fieldName : fieldNames) {
+            title += StringUtil.padRight( boarder + " " + fieldName, columnWidth);
+
+        }
+
+        String line = StringUtil.repeat("-" , columnWidth * fieldNames.size());
+        String linewith = StringUtil.repeat( "-", columnWidth - 1);
+        String linewith1 = StringUtil.repeat( "|", 1);
+        String linewith2 = StringUtil.repeat( "-", columnWidth - 4);
+
+
+
+        System.out.println(line);
+        System.out.println(title);
+        System.out.println(linewith1 + linewith + linewith1 + linewith + linewith1 + linewith2 + linewith1);
+
+
+
+
 
         for (Game game : games) {
 
-            String productLine = (game.getName())
-                    + ("," + game.getNumPlayers() + " players" )
-                    + ("," + game.getPrice() + " €");
+            String productLine = StringUtil.padRight( boarder + " " + game.getName(), columnWidth)
+                    + StringUtil.padRight( "" + boarder + " " + game.getNumPlayers(), columnWidth)
+                    + StringUtil.padRight( "" + boarder + " " + game.getPrice() + " " + boarder , columnWidth);
 
             System.out.println(productLine);
 
-    }
+        }
+        System.out.println(line);
+
+
 
     }
 }
---
+++ src/main/java/util/StringUtil.java
@@ -0,0 +1,31 @@
+package util;
+
+public class StringUtil {
+
+    /**
+     * returns a String made of s*/
+
+    public static String repeat (String s, int n) {
+
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            result += s;
+        }
+        return result;
+    }
+
+    public static String padLeft (String s, int n) {
+
+        return repeat(" ", n - s.length()) + s;
+    }
+
+    /**
+     * returns a string containing s plus spaces to the right so the resulting string has length is n
+     */
+
+    public static String padRight (String s, int n) {
+
+        return s + repeat(" ", n - s.length());
+    }
+
+}
